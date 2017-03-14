7925dc670ee79cf03f0081ee7bb7bb15d1af31d1
aanand-aditya <aanand@grepruby.com>
2017-03-14 21:21:36 +0530

    final changes

--- promisemulti.js
+++ promisemulti.js
@@ -1,33 +1,23 @@
 var Promise = require('es6-promise').Promise;
-
+var fs = require("fs");
 function readContent(s){
 
   var fs = require("fs"), f1 = "file1.txt", f2 = "file2.txt", f3 = "file3.txt";
-  var file1 = new Promise(function(resolve,reject){
-    resolve(checkFile(f1));
-    });
-  var file2 = new Promise(function(resolve,reject){
-   resolve(checkFile(f2));
-    });
-  var file3 = new Promise(function(resolve,reject){
-    resolve(checkFile(f3));
-  });
-  
     
-file1.then((val) => console.log(val))
-  .then(file2.then((val) => console.log(val)))
-  .then(file3.then((val) => console.log(val)))
-  .catch((err) => console.log(err));
-}
-
-function checkFile(file)
-{
+  
+  makePromise(f1).then(res => makePromise(f2))
+                 .then(res => makePromise(f3))
+                 .catch((err) => console.log(err));
+  }
+function makePromise(file){
+ return new Promise(function(resolve,reject){
   fs.readFile(file, function (err, data) {
     if (err) 
-      return(err);
+      reject(err);
     else
-      return(printContent(data));
+      resolve(printContent(data));
   });
+ });
 }
 
 function printContent(data){
